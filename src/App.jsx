e695b33a4567634bf5d12d2194179cd7580185eb
Subodh-Khadka <subodhkdk1100@gmail.com>
2025-08-08 14:55:32 +0545

    Set up React Dialog in Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { CartContextProvider } from "./store/CartContext";
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import { UserProgressContextProvider } from "./store/UserProgressContext";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <>
-      <CartContextProvider>
-        <Header />
-        <Meals />
-      </CartContextProvider>
+      <UserProgressContextProvider>
+        <CartContextProvider>
+          <Header />
+          <Meals />
+          <Cart />
+        </CartContextProvider>
+      </UserProgressContextProvider>
     </>
   );
 }
