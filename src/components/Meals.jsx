702ac8383a1c63c246b9ad5047e2be4bd30e74e5
Subodh-Khadka <subodhkdk1100@gmail.com>
2025-08-10 13:30:43 +0545

    Custom Hooks/Avoid erros

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,21 @@
 import MealItem from "./MealItem";
-
+import useHttp from "../hooks/useHttp";
 import { useEffect, useState } from "react";
 
+const requestConfig = {};
+
 export default function Meals() {
-  const [loadedMeals, setloadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+  console.log(loadedMeals);
 
-      if (!response) {
-      }
-
-      const meals = await response.json();
-      setloadedMeals(meals);
-    }
-
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p>Fetching Meals...</p>;
+  }
 
   return (
     <ul id="meals">
